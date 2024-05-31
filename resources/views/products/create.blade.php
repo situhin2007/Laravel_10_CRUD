22226437e480dfb94ccd01f31c7756471ae22617
siful <situhin2007@gmail.com>
2024-06-01 03:45:01 +0600

    frontend

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 CRUD</title>
+
+        <title>Bootstrap 5 Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-sm bg-dark">
+
+        <div class="container-fluid">
+          <!-- Links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-light" href="/">Products</a>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <form method="POST">
+                    
+                </form>
+            </div>
+        </div>
+        <h1>New Product</h1>
+    </div>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 CRUD</title>
+
+        <title>Bootstrap 5 Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-sm bg-dark">
+
+        <div class="container-fluid">
+          <!-- Links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-light" href="/">Products</a>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+    <div class="container">
+        <div class="text-right">
+            <a href="product/create" class="btn btn-dark mt-2">New Product</a>
+        </div>
+        <h1>Products</h1>
+    </div>
+</body>
+</html>
